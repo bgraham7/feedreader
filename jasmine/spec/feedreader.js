c240a5318336349cf358f979cfcd41734f86362f
bgraham <bgraham626@gmail.com>
2018-09-30 20:39:07 -0400

    Menu visibility tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,20 +52,32 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
-
-    });
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function() {
+            const isHidden = $('body').hasClass('menu-hidden');
+            expect(isHidden).toBe(true);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+        it('toggles visibility on click', function() {
+            const menuButton = $('.menu-icon-link');
+            const body = $('body');
+            menuButton.click();
+            let isHidden = body.hasClass('menu-hidden');
+            expect(isHidden).toBe(false);
+            menuButton.click();
+            isHidden = body.hasClass('menu-hidden');
+            expect(isHidden).toBe(true);
+        });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
