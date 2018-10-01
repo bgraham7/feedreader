5f9f7ba9705cb19c44f28985794155335b7f76b8
bgraham <bgraham626@gmail.com>
2018-09-30 20:48:45 -0400

    Test for ensuring intial feed populated entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -87,6 +87,18 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('loadFeed populates at least one item', function(done) {
+            const isHidden = $('body').hasClass('menu-hidden');
+            const entires = $(".feed").find(".entry");
+            expect(entires.length).toBeGreaterThan(0);
+            done();
+        });
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
